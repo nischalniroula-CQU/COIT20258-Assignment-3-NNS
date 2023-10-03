b4b13a28d7db9bc1dcd86ef88f856863d7cbae4f
Nischal Niroula <officialnischalniroula@gmail.com>
2023-10-04 10:54:39 +1100

    Replaced pom.xml based on Sayar's, made controller package, added login controller

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.hospitalmanagementsystem {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.hospitalmanagementsystem to javafx.fxml;
     exports com.hospitalmanagementsystem;
